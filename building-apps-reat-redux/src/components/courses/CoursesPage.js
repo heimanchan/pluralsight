2ddc672b3e16d1973857ae9b869889dd0bef8d2a
Heiman Chan <heimanchan0@gmail.com>
2022-08-16 22:08:12 -0700

    Add loadCourses thunk

--- building-apps-reat-redux/src/components/courses/CoursesPage.js
+++ building-apps-reat-redux/src/components/courses/CoursesPage.js
@@ -5,51 +5,14 @@ import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 
 class CoursesPage extends React.Component {
-  // constructor(props) {
-  //   super(props);
-
-  //   this.state = {
-  //     course: {
-  //       title: "",
-  //     },
-  //   };
-
-  //   this.handleChange = this.handleChange.bind(this);
-  // }
-  state = {
-    course: {
-      title: "",
-    },
-  };
-
-  handleChange = (event) => {
-    const course = { ...this.state.course, title: event.target.value };
-    this.setState({ course: course });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    this.props.actions.createCourse(this.state.course);
-    this.setState({
-      course: { ...this.state.course, title: "" },
-    });
-  };
-
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <>
         <h2>Courses</h2>
-        <h3>Add course</h3>
-        <input
-          type="text"
-          onChange={this.handleChange}
-          value={this.state.course.title}
-        />
-        <input type="submit" value="Save" />
         {this.props.courses.map((course) => (
           <div key={course.title}>{course.title}</div>
         ))}
-      </form>
+      </>
     );
   }
 }
