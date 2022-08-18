694aff83ce9edfe886fe3192a220d3671391adf3
Heiman Chan <heimanchan0@gmail.com>
2022-08-17 23:22:34 -0700

    Update state so course page render correctly

--- building-apps-reat-redux/src/components/courses/ManageCoursePage.js
+++ building-apps-reat-redux/src/components/courses/ManageCoursePage.js
@@ -12,6 +12,7 @@ function ManageCoursePage({
   loadAuthors,
   loadCourses,
   saveCourse,
+  history,
   ...props
 }) {
   const [course, setCourse] = useState({ ...props.course });
@@ -22,6 +23,8 @@ function ManageCoursePage({
       loadCourses().catch((error) => {
         alert("Loading courses failed " + error);
       });
+    } else {
+      setCourse({ ...props.course });
     }
 
     if (authors.length === 0) {
@@ -29,7 +32,7 @@ function ManageCoursePage({
         alert("Loading authors failed " + error);
       });
     }
-  }, []);
+  }, [props.course]);
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -41,7 +44,9 @@ function ManageCoursePage({
 
   function handleSave(event) {
     event.preventDefault();
-    saveCourse(course); //saveCourse function from props of local state
+    saveCourse(course).then(() => {
+      history.push("/courses");
+    }); //saveCourse function from props of local state
   }
 
   return (
@@ -62,11 +67,21 @@ ManageCoursePage.propTypes = {
   loadAuthors: PropTypes.func.isRequired,
   loadCourses: PropTypes.func.isRequired,
   saveCourse: PropTypes.func.isRequired,
+  history: PropTypes.object.isRequired,
 };
 
-function mapStatesToProps(state) {
+export function getCourseBySlug(courses, slug) {
+  return courses.find((course) => course.slug === slug) || null;
+}
+
+function mapStatesToProps(state, ownProps) {
+  const slug = ownProps.match.params.slug;
+  const course =
+    slug && state.courses.length > 0
+      ? getCourseBySlug(state.courses, slug)
+      : newCourse;
   return {
-    course: newCourse,
+    course: course,
     courses: state.courses,
     authors: state.authors,
   };
