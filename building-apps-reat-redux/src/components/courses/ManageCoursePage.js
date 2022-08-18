d3b07258efb1a7548f4ba872428eed2474ad5705
Heiman Chan <heimanchan0@gmail.com>
2022-08-17 22:08:37 -0700

    Add ManageCoursePage.js

---
+++ building-apps-reat-redux/src/components/courses/ManageCoursePage.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseAction";
+import * as authorActions from "../../redux/actions/authorAction";
+import PropTypes from "prop-types";
+
+class ManageCoursePage extends React.Component {
+  componentDidMount() {
+    const { courses, authors, loadAuthors, loadCourses } = this.props;
+    if (courses.length === 0) {
+      loadCourses().catch((error) => {
+        alert("Loading courses failed " + error);
+      });
+    }
+
+    if (authors.length === 0) {
+      loadAuthors().catch((error) => {
+        alert("Loading authors failed " + error);
+      });
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <h2>Manage Course</h2>
+      </>
+    );
+  }
+}
+
+ManageCoursePage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  authors: PropTypes.array.isRequired,
+  loadAuthors: PropTypes.func.isRequired,
+  loadCourses: PropTypes.func.isRequired,
+};
+
+function mapStatesToProps(state) {
+  return {
+    courses: state.courses,
+    authors: state.authors,
+  };
+}
+// Declare mapDispatchToProps as object
+const mapDispatchToProps = {
+  loadCourses: courseActions.loadCourses,
+  loadAuthors: authorActions.loadAuthors,
+};
+
+// function mapDispatchToProps(dispatch) {
+//   return {
+//     actions: {
+//       loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
+//       loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch),
+//     },
+//   };
+// }
+
+export default connect(mapStatesToProps, mapDispatchToProps)(ManageCoursePage);
